44306b9e8bf4ac422924b50aae74f62f798ad83f
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-09 00:31:26 +0100

    NextjsRouting Groupe Route

---
+++ 07-route-group/src/app/shop/[productId]/reviews/[reviewsId]/page.tsx
@@ -0,0 +1,20 @@
+import { notFound } from "next/navigation";
+
+export default function ReviewOne({params} : {
+  params : {
+    reviewsId : string ;
+    productId : string ;
+  }
+}) {
+
+  if(params.reviewsId === "admin"){
+    notFound()
+  }
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-slate-700">The product {params.productId} <br/> has review : {params.reviewsId}</h1>
+      </div>
+    );
+  }
+  
+
