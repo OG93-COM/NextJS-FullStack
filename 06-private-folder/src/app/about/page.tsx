eb45954a504769608cfc1df0a313bea18b22edab
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-09 00:29:01 +0100

    NextjsRouting Private Folder

---
+++ 06-private-folder/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+
+export default function About() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-slate-700">About Us</h1>
+    </div>
+  )
+}
---
+++ 06-private-folder/src/app/components/Nav.tsx
@@ -0,0 +1,7 @@
+import styles from '../styles/components/navigations.module.css'
+
+export default function Nav() {
+  return (
+    <div className={styles.container}>Navigation</div>
+  )
+}
