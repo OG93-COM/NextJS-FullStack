15187fcfa1b359cc72291a0c9f6fc03b47f1f5da
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-12 19:42:28 +0100

    NextJs URL Query

---
+++ 23-URL-Query-Params/src/app/reviews/data.ts
@@ -0,0 +1,14 @@
+export const reviewTab:{id:number, text:string}[] = [
+    {
+        id:1,
+        text:"First Review"
+    },
+    {
+        id:2,
+        text:"Second Review"
+    },
+    {
+        id:3,
+        text:"Third Review"
+    }
+]
