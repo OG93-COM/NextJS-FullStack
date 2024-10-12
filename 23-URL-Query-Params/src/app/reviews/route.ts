b2d92c2f1ea31d2de8891135cafc9092d2611df2
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-12 19:44:00 +0100

    NextJs URL Query

--- 23-URL-Query-Params/src/app/reviews/route.ts
+++ 23-URL-Query-Params/src/app/reviews/route.ts
@@ -1,13 +1,10 @@
 import {type NextRequest, NextResponse } from "next/server";
 import { reviewTab } from "./data";
 
-
-
 export async function GET(req: NextRequest) {
 
         const searchParams = req.nextUrl.searchParams;
         const query = searchParams.get('query')
         const filtredReview = query ? reviewTab.filter(rev => rev.text.includes(query)) : reviewTab;
         return NextResponse.json(filtredReview)
-
 }
