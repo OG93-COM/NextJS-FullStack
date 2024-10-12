fd6f74cbfb8e237d9a21f07e285ec2f9694377de
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-12 19:07:05 +0100

    NextJs PATCH Req

--- 21-PATCH-request/src/app/reviews/[reviewId]/route.ts
+++ 21-PATCH-request/src/app/reviews/[reviewId]/route.ts
@@ -2,25 +2,23 @@ import {NextResponse } from "next/server";
 import { reviewTab } from "../data";
 
 export async function GET(_req:Request, {params}:{params : {reviewId:string}}) {
-    
+
     try {
         const review = reviewTab.find(rev => rev.id === parseInt(params.reviewId))
         return NextResponse.json(review,{status:200})
     } catch (error) {
         return NextResponse.json({message:"Review not found"}, {status:500})
     }
-    
 }
 
 export async function PATCH(req:Request, {params}: {params :{reviewId : string}}) {
-    
+    const {text} = await req.json()
     try {
-    const {id, text} = await req.json()
-    const idRev = params.reviewId
-    const index = reviewTab.findIndex(rev => rev.id === parseInt(idRev) )
+    const index = reviewTab.findIndex(rev => rev.id === parseInt(params.reviewId) )
     reviewTab[index].text = text
     return NextResponse.json(reviewTab)
     } catch (error) {
         return NextResponse.json({message:"Review Not Updated!!!"},{status:405})
     }
 }
+
