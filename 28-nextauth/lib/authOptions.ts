d88937131cefc659af82d269e7bc8e0fc529fbe2
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-16 23:32:35 +0100

    NextJs NextAuth

---
+++ 28-nextauth/lib/authOptions.ts
@@ -0,0 +1,21 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions ={
+    providers:[
+        GithubProvider({
+            clientId: process.env.CLIENT_ID_GITHUB as string,
+            clientSecret: process.env.CLIENT_SECRET_GITHUB as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE as string,
+            clientSecret: process.env.CLIENT_SECRET_GOOGLE as string,
+        })
+
+    ],
+    pages: {
+        signIn:"/login"
+    },
+    secret: process.env.NEXTAUTH_SECRET as string
+}
