4ae07058d5353440b7df43d90b5a0399c59bad3b
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-16 01:49:48 +0100

    NextJs NextAuth

--- 28-nextauth/src/app/layout.tsx
+++ 28-nextauth/src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Nav/>
         {children}
         <Footer/>
