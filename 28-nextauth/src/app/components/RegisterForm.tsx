ee191970e9b0908c7c8d199fac7a29df4d19971b
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-18 02:19:45 +0100

    NextJs NextAuth

--- 28-nextauth/src/app/components/RegisterForm.tsx
+++ 28-nextauth/src/app/components/RegisterForm.tsx
@@ -80,6 +80,7 @@ export default function RegisterForm() {
             Password
           </label>
           <input
+            {...register('password')}
             type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="••••••••"/>
@@ -90,7 +91,7 @@ export default function RegisterForm() {
             Confirm password
           </label>
           <input
-          {...register('password')}
+            {...register('confirmPassword')}
             type="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="••••••••"/>
