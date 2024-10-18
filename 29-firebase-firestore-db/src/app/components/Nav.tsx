2c34383ce40e722cd02a71073f5a59fcd39629d0
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-18 19:57:33 +0100

    NextJs Firebase Firestore DB

--- 29-firebase-firestore-db/src/app/components/Nav.tsx
+++ 29-firebase-firestore-db/src/app/components/Nav.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div className="h-12 p-3 bg-slate-700 text-white flex items-center gap-3 shadow-md">
+        
+        <Link href={"/"}>Firestore Database</Link>
+        <Link href={"/realtime"}>Realtime</Link>
+        <Link href={"/signInAndUp"}>Login</Link>
+        <Link href={"/dashboard"}>Dashboard</Link>
+    </div>
   )
 }
--- 29-firebase-firestore-db/src/app/layout.tsx
+++ 29-firebase-firestore-db/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./styles/globals.css";
+import Nav from "./components/Nav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+        <Nav/>
         {children}
       </body>
     </html>
