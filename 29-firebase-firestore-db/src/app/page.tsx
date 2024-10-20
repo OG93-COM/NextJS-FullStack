6579be71be97f1ac4e8b2f0ba58655b86ee24621
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-21 00:02:03 +0100

    NextJs Firebase Firestore DB

--- 29-firebase-firestore-db/src/app/page.tsx
+++ 29-firebase-firestore-db/src/app/page.tsx
@@ -5,7 +5,6 @@ import { useFirebase } from "./context/dataContext";
 export default function Home() {
 
   const {members} = useFirebase()
-  console.log(members)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
