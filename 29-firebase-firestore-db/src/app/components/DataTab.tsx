f3cf7665cf98ab6212b14b3a0e49b1670db98cc1
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-21 20:20:38 +0100

    NextJs Firebase Firestore DB

--- 29-firebase-firestore-db/src/app/components/DataTab.tsx
+++ 29-firebase-firestore-db/src/app/components/DataTab.tsx
@@ -7,17 +7,17 @@ export default function DataTab() {
     <table className="shadow-lg rounded-lg divide-x divide-y">
       <thead className="bg-slate-800 text-white rounded-lg">
         <tr>
-          <th className="p-3 text-sm border">Id</th>
-          <th className="p-3 text-sm border">Image</th>
-          <th className="p-3 text-sm border">First Name</th>
-          <th className="p-3 text-sm border">Last Name</th>
-          <th className="p-3 text-sm border">Email</th>
-          <th className="p-3 text-sm border">Phone</th>
-          <th className="p-3 text-sm border">Adress</th>
-          <th className="p-3 text-sm border">Postal</th>
-          <th className="p-3 text-sm border">City</th>
-          <th className="p-3 text-sm border">Country</th>
-          <th className="p-3 text-sm border">Actions</th>
+          <th className="p-2 text-sm border">Id</th>
+          <th className="p-2 text-sm border">Image</th>
+          <th className="p-2 text-sm border">First Name</th>
+          <th className="p-2 text-sm border">Last Name</th>
+          <th className="p-2 text-sm border">Email</th>
+          <th className="p-2 text-sm border">Phone</th>
+          <th className="p-2 text-sm border">Adress</th>
+          <th className="p-2 text-sm border">Postal</th>
+          <th className="p-2 text-sm border">City</th>
+          <th className="p-2 text-sm border">Country</th>
+          <th className="p-2 text-sm border">Actions</th>
         </tr>
       </thead>
       <tbody>
--- 29-firebase-firestore-db/src/app/page.tsx
+++ 29-firebase-firestore-db/src/app/page.tsx
@@ -9,9 +9,10 @@ export default function Home() {
 
   return (
     <div className="mx-auto flex flex-col items-center justify-items-center min-h-screen p-2 ">
-      <div className="flex items-center gap-2 my-2 py-2 px-4 text-sm rounded-xl bg-gray-100 hover:bg-gray-200 duration-200 cursor-pointer">
+      <button
+      className="flex items-center gap-2 my-2 py-2 px-4 text-sm rounded-xl bg-gray-100 hover:bg-gray-200 duration-200">
         Add New Member <GrUserNew/>
-      </div>
+      </button>
       <DataTab/>
     </div>
   );
