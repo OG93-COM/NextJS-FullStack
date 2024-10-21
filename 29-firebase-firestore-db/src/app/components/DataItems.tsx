4d5427d831438756de732e915684434560e0f84c
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-21 19:15:49 +0100

    NextJs Firebase Firestore DB

--- 29-firebase-firestore-db/src/app/components/DataItems.tsx
+++ 29-firebase-firestore-db/src/app/components/DataItems.tsx
@@ -1,6 +1,31 @@
+import { useFirebase } from "../context/dataContext";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
 
 export default function DataItems() {
+  const { members } = useFirebase();
   return (
-    <div>DataItems</div>
+  <>
+  {members && members.map((member) => (
+      <tr>
+      <td>{member.id}</td>
+      <td>{member.image}</td>
+      <td>{member.firstName}</td>
+      <td>{member.lastName}</td>
+      <td>{member.email}</td>
+      <td>{member.phone}</td>
+      <td>{member.adress}</td>
+      <td>{member.cp}</td>
+      <td>{member.city}</td>
+      <td>{member.country}</td>
+      <td>
+      <div className="flex justify-center items-center gap-2">
+      <FaEdit />
+      <FaTrash />
+      </div>
+      </td>
+      </tr>
   )
+  )}
+  </>)
 }
--- 29-firebase-firestore-db/src/app/components/DataTab.tsx
+++ 29-firebase-firestore-db/src/app/components/DataTab.tsx
@@ -1,13 +1,27 @@
-import { useFirebase } from "../context/dataContext"
-import DataItems from "./DataItems"
-
+import DataItems from "./DataItems";
 
 export default function DataTab() {
-    const {members} = useFirebase()
-    return (
-    <div>DataTab
-
-        <DataItems/>
-    </div>
-  )
+  
+  return (
+    <table className="table-auto shadow-lg rounded-lg  divide-x divide-y">
+      <thead className="bg-slate-800 text-white rounded-lg">
+        <tr>
+          <th className="p-3 text-sm border">Id</th>
+          <th className="p-3 text-sm border">Image</th>
+          <th className="p-3 text-sm border">First Name</th>
+          <th className="p-3 text-sm border">Last Name</th>
+          <th className="p-3 text-sm border">Email</th>
+          <th className="p-3 text-sm border">Phone</th>
+          <th className="p-3 text-sm border">Adress</th>
+          <th className="p-3 text-sm border">Postal</th>
+          <th className="p-3 text-sm border">City</th>
+          <th className="p-3 text-sm border">Country</th>
+          <th className="p-3 text-sm border">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <DataItems />
+      </tbody>
+    </table>
+  );
 }
