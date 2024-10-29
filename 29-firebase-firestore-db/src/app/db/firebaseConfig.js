bdc5f83ebe23eae37130eaa2758283047988f04c
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-29 01:38:58 +0100

    NextJs Firebase RealTime DB

--- 29-firebase-firestore-db/src/app/db/firebaseConfig.js
+++ 29-firebase-firestore-db/src/app/db/firebaseConfig.js
@@ -2,15 +2,24 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 import {getStorage} from  'firebase/storage'
 
+//Realtime Database
+import { getDatabase } from "firebase/database";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
+  //Realtime Database
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const storage = getStorage(app)
+
+//Realtime Database
+export const database = getDatabase(app)
