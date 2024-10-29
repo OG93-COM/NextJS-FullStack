dbb1eaf79e73158435c2659f5a8bd9beaae2dd74
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-30 00:14:23 +0100

    NextJs Firebase RealTime DB

--- 29-firebase-firestore-db/src/app/realtime/page.tsx
+++ 29-firebase-firestore-db/src/app/realtime/page.tsx
@@ -1,10 +1,11 @@
 
 export default function Realtime() {
   return (
-    <div className="p-4">
-      <form>
+    <div className="p-4 max-w-[600px] mx-auto">
+      <form className="mb-8">
         <label>Add New Todo to Realtime Database from Firebase</label>
-        <input type="text" placeholder="add todo here ..." className="border"/>
+        <input type="text" placeholder="add todo here ..." className="w-full border p-2 my-2 rounded-md"/>
+        <button type="submit" className="px-4 py-2 bg-blue-500 rounded-md text-white hover:bg-blue-600 w-full" >Add New Todo</button>
       </form>
     </div>
   )
