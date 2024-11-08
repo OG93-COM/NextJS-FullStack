4a6740b5a9f42459eed6c42795b4f318b9915745
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-11-09 00:13:40 +0100

    NextJs Firebase RealTime DB

--- 29-firebase-firestore-db/src/app/error.tsx
+++ 29-firebase-firestore-db/src/app/error.tsx
@@ -2,9 +2,11 @@
 import React, { useEffect } from 'react'
 
 export default function error({error, reset} : {error:Error, reset:()=> void}) {
+
     useEffect(()=>{
         console.log(error)
     },[error])
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
         <h2 className='text-xl text-red-600'>Oops, Error</h2>
