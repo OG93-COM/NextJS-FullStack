006c4090f096d9eb9426f70ce9a2a8f7d8c46bb3
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-11-10 20:20:40 +0100

    NextJs CRUD File

--- 25-crud-local-file/src/app/users/[id]/page.tsx
+++ 25-crud-local-file/src/app/users/[id]/page.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+"use client"
+import { User } from '@/app/types/types'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState, useTransition } from 'react'
 
 export default function page() {
+    const [user, setUser] = useState<User[]>([])
+    const [errors, setErrors] = useState<string | null>(null)
+    const [isPending, startTransition] = useTransition()
+    const param = useParams()
+    const id = param.id
+
+    useEffect(()=>{
+        const fetchUserById = async () => {
+            try {
+                startTransition(async ()=>{
+                    const response = await fetch(`/api/getUserById?id=${id}`)
+                    if(!response.ok){
+                        throw new Error("error when fetching the data")
+                    }
+                    const data : User = await response.json();
+                    setUser(data)
+                    console.log(user)
+                })
+            } catch (err : any) {
+                setErrors(err.message)
+            }
+        }
+
+        fetchUserById()
+    },[])
+    
   return (
-    <div>page user by ID</div>
+    <div>User  ID : {id}</div>
   )
 }
